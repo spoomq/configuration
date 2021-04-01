5c6c74e7ab8e6401d51e1a3009a0742d6cad7fe1
spoomq <spoomq@gmail.com>
2021-04-01 16:41:40 -0400

    add xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,17 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.CustomKeys
+import XMonad.Util.EZConfig
+
+main = do
+xmproc <- spawnPipe "xmobar"
+xmonad $ defaultConfig
+  { modMask = mod4Mask
+  , terminal = "alacritty"
+  , borderWidth = 2
+  , manageHook = manageDocks <+> manageHook defaultConfig
+  , layoutHook = avoidStruts $ layoutHook defaultConfig
+  , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn xmproc, ppOrder = \(ws:_:t:_) -> [ws,t] }
+  }
