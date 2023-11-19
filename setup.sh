fda17769d622236e580312e62d27c75dd98aacb4
spoomq <spoomq@gmail.com>
2023-11-18 22:16:05 -0500

    setup.sh fixes

--- setup.sh
+++ setup.sh
@@ -3,62 +3,66 @@
 set -e
 
 # updates & installs
-echo "updating system..."
+echo -e "updating system..."
 sudo apt update && sudo apt upgrade -y
 
-echo "installing packages..."
+echo -e "installing packages..."
 sudo add-apt-repository universe
-sudo apt install build-essential, vim, git, net-tools, gnome-tweaks, neofetch, cmatrix
+sudo apt install build-essential vim git net-tools gnome-tweaks neofetch cmatrix -y
 
-echo "removing & installing snap packages..."
+echo -e "removing & installing snap packages..."
 sudo snap remove --purge firefox
 sudo snap install chromium spotify
 
-echo "installing alacritty..."
+echo -e "installing alacritty..."
 sudo add-apt-repository ppa:aslatter/ppa -y
-sudo apt install alacritty
+sudo apt install alacritty -y
 
-echo "remove packages..."
-sudo apt remove gnome-shell-extention-ubuntu-dock gnome-terminal
-sudo apt autoremove
+echo -e "remove packages..."
+sudo apt remove gnome-shell-extension-ubuntu-dock gnome-terminal -y
+sudo apt autoremove -y
 
 # .bashrc
 sleep 1
-echo "replacing .bashrc...\n"
+echo -e "replacing .bashrc...\n"
 rm -f ~/.bashrc
 cp ./.bashrc ~
 
 # vim
-echo "moving vim config...\n"
+echo -e "moving vim config...\n"
 cp ./.vimrc ~/.vimrc;
 
 # tmux
 sleep 1
-echo "moving tmux config...\n"
+echo -e "moving tmux config...\n"
 cp ./.tmux.conf ~/.tmux.conf;
 
 # alacritty
 sleep 1
-echo "moving alacritty config...\n"
+echo -e "moving alacritty config...\n"
 mkdir ~/.config/alacritty
 cp ./.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml;
 
+# backgrounds
+echo -e "moving backgrounds...\n"
+cp ./backgrounds/* ~/.local/share/backgrounds/
+
 # system
-echo "changing background...\n"
-gsettings set org.gnome.desktop.background picture-uri file:///home/user/.local/share/backgrounds/horse.jpg
+echo -e "changing background...\n"
+gsettings set org.gnome.desktop.background picture-uri file:///home/user/.local/share/backgrounds/horse.png
 
 sleep 1
-echo "changing theme...\n"
+echo -e "changing theme...\n"
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 
 sleep 2
-echo "done.\n"
+echo -e "done.\n"
 
-read -p "would you like to logout? " -n 1 -r
+read -p "would you like to reboot?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  logout
+  reboot
 else
-  echo "ok."
+  echo ok.
   return
 fi
