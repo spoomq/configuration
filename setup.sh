2adfe5313171e1fc7120067157bb6373b5e5adb3
spoomq <spoomq@gmail.com>
2025-02-13 00:58:00 -0500

    fix modernization

--- setup.sh
+++ setup.sh
@@ -8,17 +8,30 @@ sudo apt update && sudo apt upgrade -y
 
 echo -e "installing packages..."
 sudo add-apt-repository universe
-sudo apt install build-essential vim git net-tools gnome-tweaks neofetch cmatrix -y
+sudo apt install build-essential curl vim git net-tools gnome-tweaks neofetch cmatrix -y
 
 echo -e "removing & installing snap packages..."
 sudo snap remove --purge firefox
 sudo snap install chromium spotify
 
 echo -e "installing alacritty..."
-sudo add-apt-repository ppa:aslatter/ppa -y
-sudo apt install alacritty -y
-mkdir -p ~/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty && pwd
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env
+rustup override set stable
+rustup update stable
+sudo apt install cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+cargo build --release
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+sudo cp ./target/release/alacritty /usr/bin/
+#sudo cp ./extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+#sudo cp ./extra/linux/Alacritty.desktop /usr/share/applications
+#sudo update-desktop-database
+rustup self uninstall
+cd - && pwd
+sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
+echo -e "alacritty done."
 
 echo -e "remove packages..."
 sudo apt remove gnome-shell-extension-ubuntu-dock gnome-terminal -y
@@ -42,8 +55,9 @@ cp ./.tmux.conf ~/.tmux.conf;
 # alacritty
 sleep 1
 echo -e "moving alacritty config...\n"
-mkdir ~/.config/alacritty
+mkdir -p ~/.config/alacritty/themes
 cp ./alacritty.toml ~/.config/alacritty/;
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 # backgrounds
 # echo -e "moving backgrounds...\n"
